fc24ba7e95a3765fadf8ba5de278b7f3585c974d
andrija-mitrovic <andrija.mitrovic9@gmail.com>
2021-11-07 08:56:37 +0100

    DeleteOrderCommand handler added

---
+++ src/Services/Ordering/Ordering.Application/Features/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Ordering.Application.Contracts.Persistence;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Features.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler : IRequestHandler<DeleteOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IMapper _mapper;
+        private readonly ILogger<DeleteOrderCommandHandler> _logger;
+
+        public DeleteOrderCommandHandler(IOrderRepository orderRepository,
+            IMapper mapper,
+            ILogger<DeleteOrderCommandHandler> logger)
+        {
+            _orderRepository = orderRepository;
+            _mapper = mapper;
+            _logger = logger;
+        }
+        public async Task<Unit> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+            var orderToDelete =await _orderRepository.GetByIdAsync(request.Id);
+
+            if (orderToDelete == null)
+            {
+                _logger.LogError("Order not exist in database.");
+                //throw new NotFound(nameof(Order), request.Id);
+            }
+
+            await _orderRepository.DeleteAsync(orderToDelete);
+
+            _logger.LogInformation($"Order {orderToDelete.Id} is successfully deleted.");
+
+            return Unit.Value;
+        }
+    }
+}
